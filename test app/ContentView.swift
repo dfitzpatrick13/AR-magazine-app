8a201be9dcbf2a3efd73a8d6a026bfb56af18d65
Daniel Fitzpatrick <danfitz156@gmail.com>
2023-01-11 14:14:08 -0500

    Initial Commit

---
+++ test app/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  test app
+//
+//  Created by Daniel Fitzpatrick on 1/2/23.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let anchor = try! Magazine.loadScene()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(anchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
